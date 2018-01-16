b2ec2d6941f56b4290126c271013d8700493251c
ziginsider <ziginsider@gmail.com>
2018-01-16 14:10:53 +0300

    work out list

--- app/src/main/java/io/github/ziginsider/kotlindiffutil/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/kotlindiffutil/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import io.github.ziginsider.kotlindiffutil.adapter.UserAdapter
+import io.github.ziginsider.kotlindiffutil.mock.MockDataProvider
 import io.github.ziginsider.kotlindiffutil.model.User
 import io.github.ziginsider.kotlindiffutil.utils.toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,14 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val users = MockDataProvider().data
 
+        updateAdapter(users)
     }
 
     private fun updateAdapter(userList: List<User>) {
         recyclerAdapter?.update(userList) ?: setUpRecyclerView(userList)
     }
 
-    private fun setUpRecyclerView(userList: List<User) {
+    private fun setUpRecyclerView(userList: List<User>) {
         recyclerAdapter = UserAdapter(userList, {toast("ddd")})
         with(recyclerView) {
             layoutManager = LinearLayoutManager(this.context)
